880c9160790d1b1772b4f441ca6c80b9cfaac2a7
Daniel De La Pava Suarez <daniel.samkit@gmail.com>
2020-01-29 13:16:07 -0500

    add logic initial

---
+++ cmd/bacon-ipsum/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/samuskitchen/go-cli-simple/generator/bacon"
+)
+
+var appName = "bacon-ipsum"
+
+func main() {
+	textTypePtr := flag.String("type", "", "Type of the text to generate (Required) [Valid options: all-meat, meat-and-filler]")
+	parasPtr := flag.Int("paras", 5, "number of paragraphs")
+	sentencesPtr := flag.Int("sentences", 0, "number of sentences (this overrides paragraphs)")
+	withLorem := flag.Bool("withLorem", false, "if it is true the first paragraph start with 'Bacon dolor sit amet'")
+
+	flag.Usage = usage
+
+	flag.Parse()
+
+	// Parsing textTypePtr to type TextType
+	textType := bacon.TextType(*textTypePtr)
+
+	// Check if is a valid textType or empty
+	if !textType.Validate() {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	// Initialize the new struct bacon Ipsum with the input data
+	g := bacon.NewBaconIpsum(
+		textType,
+		*parasPtr,
+		*sentencesPtr,
+		*withLorem)
+
+	// Generate the text calling to the selected strategy
+	fmt.Println(g.GenerateText())
+}
+
+func usage() {
+	msg := fmt.Sprintf(`usage: %s [OPTIONS]
+	%s is a simple tool to generate random text based on a bacon ipsum API
+	`, appName, appName)
+	fmt.Println(msg)
+	flag.PrintDefaults()
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/samuskitchen/go-cli-simple
 
 go 1.13
-
-require github.com/friendsofgo/bacon-ipsum v0.0.0-20190120214925-e4d90e3dc8e4 // indirect
